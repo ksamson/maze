593ed442f060fdbf80e53f978f093932cc3d4320
kevin <909225+ksamson@users.noreply.github.com>
2020-04-23 21:10:41 +0900

    Finalize code and add animation

--- maze.py
+++ maze.py
@@ -2,35 +2,59 @@
 
 from grid import Grid
 from PIL import Image, ImageDraw
+import random
+import imageio
+import os
 
 def main():
 	# Creates a grid object with row x col cell objects
-	g = Grid(3,3, cell_size=50)
-	g.draw_grid()
-	_test(g.grid)
+	g = Grid(10,10, cell_size=100)
+	frame = 0
+	filenames = []
+	#_test(g.grid)
 
 	# Pseudocode : https://en.wikipedia.org/wiki/Maze_generation_algorithm#Recursive_backtracker
-	# stack = []
-	# initial_cell = g[0] 
-	# stack.append(initial_cell)
+	stack = []
+	initial_cell = g.grid[0]
+	initial_cell.visited = True
+	stack.append(initial_cell)
 
-	# while len(stack) != 0:
-	# 	current_cell = stack.pop()
-	# 	# Step 1: Push the current cell to the stack (if there are unvisited neighbors)
-	# 	if current_cell.check_neighbors() == True:
-	# 		stack.append(current_cell)
+	while len(stack) != 0:
+		current_cell = stack.pop()
 
-	# 		# Step 2: Choose one of the unvisited neighbors
-	#		# TODO: Implement below function
-	# 		unvisited_neighbor_cell = current_cell.get_unvisited_neighbor()
+		neighbors = current_cell.get_unvisited_neighbor()
+
+		# Step 1: Push the current cell to the stack (if there are unvisited neighbors)
+		if neighbors:
+			stack.append(current_cell)
+
+			# Step 2: Choose one of the unvisited neighbors
+			unvisited_neighbor_cell = random.choice(neighbors)
 			
-	#  		# Step 3: Remove the wall between the current and chosen cell
-	#		# TODO: Implement below function
-	#  		# current_cell.remove_wall(unvisited_neighbor_cell)
+	 		# Step 3: Remove the wall between the current and chosen cell
+			current_cell.remove_wall(unvisited_neighbor_cell)
 			
-	# 		# Step 4: Mark the chosen cell as visited and push it into the stack
-	# 		un.visited = True
-	# 		stack.append(un)
+			# Step 4: Mark the chosen cell as visited and push it into the stack
+			unvisited_neighbor_cell.visited = True
+			stack.append(unvisited_neighbor_cell)
+
+			# Below are for gif creation
+			frame += 1
+			g.draw_grid(str(frame))
+			filenames.append("./exports/maze"+str(frame)+".png")
+	
+	create_gif(filenames, 'recursive_backtracker.gif', 5)
+	g.draw_grid()
+
+def create_gif(filenames, moviename, fps=10):
+    frames = []
+    # Create gif
+    for filename in filenames:
+        frames.append(imageio.imread(filename))
+    imageio.mimsave('./exports/' + moviename, frames, fps=fps)
+    # Clean up directory
+    for filename in filenames:
+        os.remove(filename)
 
 # TODO: create a unit test
 def _test(grid):
